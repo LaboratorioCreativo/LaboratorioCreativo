070e8cbd54dd1278a131f98b0b4829aec31fc09a
Laboratorio Creativo <82228347+LaboratorioCreativo@users.noreply.github.com>
2021-04-13 15:08:48 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,21 +12,3 @@ document.querySelector("button").addEventListener("click", function() {
         clicked = true;
     }
 });
-
-
-/*function expand() {
-        document.getElementById("wrapper").style.transition = "height 2s ease 1s";
-    if (clicked) {
-        document.getElementById("wrapper").style.height = "fit-content";
-        document.getElementById("intro").style.overflow = "visible";
-        document.getElementById("arrow").style.transform = "rotate(180deg)";
-        document.getElementById("espandi").innerHTML = "Restringi";
-        clicked = false;
-    } else {
-        document.getElementById("wrapper").style.height = "50px";
-        document.getElementById("intro").style.overflow = "hidden";
-        document.getElementById("arrow").style.transform = "rotate(0deg)";
-        document.getElementById("espandi").innerHTML = "Espandi";
-        clicked = true;
-    }
-};*/
