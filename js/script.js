92af135214e066a7751ac6ebfc92796557eb129b
Laboratorio Creativo <82228347+LaboratorioCreativo@users.noreply.github.com>
2021-08-10 11:04:50 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
-let clicked = true
 
-document.querySelector("button").addEventListener("click", function() {
+document.querySelector("button").addEventListener("click", () => {
+    
+    let clicked = true
+    
     document.querySelector(".contentWrapper").classList.toggle("expand");
     if (clicked) {
         document.getElementById("arrow").style.transform = "rotate(180deg)";
@@ -11,12 +13,14 @@ document.querySelector("button").addEventListener("click", function() {
         document.getElementById("espandi").innerHTML = "Per saperne di piú clicca quí";
         clicked = true;
     }
+    preventDefault()
 });
 
-let toggled = true
+
 
 document.querySelector(".grid-toggle").addEventListener("click", () => {
     
+    let toggled = true
 
     document.querySelector(".gridGallery").classList.toggle("grid-list");
     if(toggled) {
@@ -36,4 +40,5 @@ document.querySelector(".grid-toggle").addEventListener("click", () => {
         }
         toggled = true;
     }
+    preventDefault()
 })
